ae8e785ad25d396a5ef8790f67b743f993dd7282
Ailiartsua <ailiartsuaL2@gmail.com>
2023-01-05 11:25:05 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 10.00 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i =1; i<food.length; i++){
+                for(int j=0; j<food[i]/2; j++){
+                    answer += i;
+                }    
+           }
+    StringBuilder sb = new StringBuilder();
+    sb.append(answer);
+    answer += 0;
+    answer += sb.reverse();
+    return answer;
+    }
+}
