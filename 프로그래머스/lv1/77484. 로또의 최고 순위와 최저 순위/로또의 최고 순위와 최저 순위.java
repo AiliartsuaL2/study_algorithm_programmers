215fe941837424fa10b1a9f45d3ac790be52f31b
Ailiartsua <ailiartsuaL2@gmail.com>
2023-01-05 11:25:07 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int count = 0;
+        int zeroCount = 0;
+        for(int i = 0; i<lottos.length; i++){
+            if(lottos[i] == 0){
+                zeroCount ++;
+            }
+            for(int j=0; j<win_nums.length; j++){
+                if(lottos[i]==win_nums[j] && !(lottos[i] == 0)){
+                    count++;
+                } 
+            }
+        }
+        switch(zeroCount+count){
+            case 6 : answer[0] = 1;
+                break;
+            case 5 : answer[0] = 2;
+                break;
+            case 4 : answer[0] = 3;
+                break;
+            case 3 : answer[0] = 4;
+                break;
+            case 2 : answer[0] = 5;
+                break;
+            case 1 : answer[0] = 6;
+                break;
+            case 0 : answer[0] = 6;
+                break;
+        }
+        
+        switch(count){
+            case 6 : answer[1] = 1;
+                break;
+            case 5 : answer[1] = 2;
+                break;
+            case 4 : answer[1] = 3;
+                break;
+            case 3 : answer[1] = 4;
+                break;
+            case 2 : answer[1] = 5;
+                break;
+            case 1 : answer[1] = 6;
+                break;
+            case 0 : answer[1] = 6;
+                break;
+        }
+        
+        return answer;
+    }
+}
