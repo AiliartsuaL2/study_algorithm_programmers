5e015eacc23f329f964a435282453dd7b4714536
Juho Lee <ailiartsuaL2@gmail.com>
2023-09-27 23:01:29 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.35 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,37 +1,42 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        if(s.contains("zero")){
-            s = s.replace("zero","0");
+        int startIdx = 0;
+        Map<String,Integer> map = new HashMap<String,Integer>(){
+            {
+                put("zero",0);
+                put("one",1);
+                put("two",2);
+                put("three",3);
+                put("four",4);
+                put("five",5);
+                put("six",6);
+                put("seven",7);
+                put("eight",8);
+                put("nine",9);
+            }
+        };
+        
+        StringBuilder sb = new StringBuilder();
+        for(int endIdx=0; endIdx< s.length(); endIdx++){
+            // 숫자면 그대로 더하고, 다음 수부터 시작 
+            if(Character.isDigit(s.charAt(endIdx))){
+                sb.append(s.charAt(endIdx));
+                startIdx = endIdx+1;
+                continue;
+            }
+            // 아니면 알파벳 확인
+            String numStr = s.substring(startIdx,endIdx+1);
+            // 선언되어있는 단어가 아니면 넘어감
+            int number = map.getOrDefault(numStr,-1);
+            if(number == -1)
+                continue;
+            sb.append(number);
+            startIdx = endIdx + 1;
         }
-        if(s.contains("one")){
-            s = s.replace("one","1");
-        }
-        if(s.contains("two")){
-            s = s.replace("two","2");
-        }
-        if(s.contains("three")){
-            s = s.replace("three","3");
-        }
-        if(s.contains("four")){
-            s = s.replace("four","4");
-        }
-        if(s.contains("five")){
-            s = s.replace("five","5");
-        }
-        if(s.contains("six")){
-            s = s.replace("six","6");
-        }
-        if(s.contains("seven")){
-            s = s.replace("seven","7");
-        }
-        if(s.contains("eight")){
-            s = s.replace("eight","8");
-        }
-        if(s.contains("nine")){
-            s = s.replace("nine","9");
-        }
-        answer = Integer.parseInt(s);
+        answer = Integer.parseInt(sb.toString());
         
         return answer;
     }
