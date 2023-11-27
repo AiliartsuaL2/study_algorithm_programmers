82134192948c986cc835995c850891d6aded48fe
Juho Lee <ailiartsuaL2@gmail.com>
2023-11-27 22:05:34 +0900

    [level 2] Title: 하노이의 탑, Time: 2.15 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -3,15 +3,18 @@ import java.util.ArrayList;
 
 class Solution {
     public int[][] solution(int n) {
+        List<int[]> process = new ArrayList<>();
         // n개의 기둥을 1번부터 3번까지 이동
-        return recursion(n, 1, 3).toArray(new int[0][]);
+        recursion(n, 1, 3, process);
+        return process.toArray(new int[0][]);
     }
     
-    private List<int[]> recursion(int n, int from, int to) {
+    private void recursion(int n, int from, int to, List<int[]> proccess) {
         // 종료조건 
         // 1일때는 from에서 to로 이동
         if(n == 1) {
-            return List.of(new int[] {from, to});
+            proccess.add(new int[] {from, to});
+            return;
         }
         
         // 비어있는 기둥 , 1번 2번 3번의 합에서 이동한 기둥을 빼주면 된다.
@@ -23,11 +26,13 @@ class Solution {
         //  3. n-1개의 원판을 빈 기둥에서 to 기둥으로 옮긴다.
         // pseudo code
         // (n, from, to) = 1번 (n-1, from, empty) + 2번 (1, from, to) + 3번(n-1, empty, to)
-        List<int[]> result = new ArrayList<>();
-        result.addAll(recursion(n-1, from, empty));
-        result.addAll(recursion(1, from, to));
-        result.addAll(recursion(n-1, empty, to));
+        // proccess.addAll(recursion(n-1, from, empty));
+        // proccess.addAll(recursion(1, from, to));
+        // proccess.addAll(recursion(n-1, empty, to));
         
-        return result;
+        // 최적화 - 매개변수에 결과를 더해주는 방식
+        recursion(n-1, from, empty, proccess);
+        recursion(1, from, to, proccess);
+        recursion(n-1, empty, to, proccess);
     }
 }
