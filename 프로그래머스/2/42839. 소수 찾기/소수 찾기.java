705981b4679c1d8328256ce0cd04e3496c70f3d5
Juho Lee <ailiartsuaL2@gmail.com>
2023-11-30 23:13:36 +0900

    [level 2] Title: 소수 찾기, Time: 1.99 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -4,28 +4,32 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(String numbers) {
         Set<Integer> primes = new HashSet<>();
-        List<Integer> numberList = Arrays.asList(numbers.split("")).stream()
-            .map(n -> Integer.parseInt(n))
-            .collect(Collectors.toList());
-        recursion(0, numberList, primes);
+        int[] numberArr = numbers.chars()
+            .map(n -> n - '0')
+            .toArray();
+        
+        recursion(0, numberArr, primes, new boolean[numberArr.length]);
+        
         return primes.size();
     }
     
-    private void recursion(int now, List<Integer> numbers, Set<Integer> primes) {
+    private void recursion(int now, int[] numbers, Set<Integer> primes, boolean[] isUsed) {
         // 소수면 현재 값 추가
         if(isPrime(now)) {
             primes.add(now);
         }
         // 종료조건이 없어도, 리스트가 비어있으면 아래 반복문이 실행되지 않음.
         
-        for(int i = 0; i < numbers.size(); i++) {
-            int next = (now * 10) + numbers.get(i);
-            // 전이되는 상태의 숫자를 제거,,
-            // 기존 리스트 복사 후 삭제
-            List<Integer> nextNumbers = new ArrayList<>(numbers);
-            nextNumbers.remove(i);
-            // 리턴된 값들을 결과 집합에 추가
-            recursion(next, nextNumbers, primes);
+        for(int i = 0; i < numbers.length; i++) {
+            if(isUsed[i]) {
+                continue;
+            }
+            // 백트래킹
+            isUsed[i] = true;
+            int next = (now * 10) + numbers[i];
+            recursion(next, numbers, primes, isUsed);
+            // 다음 반복문에서는 영향 없도록 false 처리
+            isUsed[i] = false;
         }
     }
     
